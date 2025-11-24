6419fe77724393f2769998fe31e09175435853b2
Tuhina-Paria <pariatuhina04@gmail.com>
2025-11-24 17:00:13 +0530

    feat(auth & products): implement complete user and seller authentication, product management setup, and add cart controller with cart routes

--- server/server.js
+++ server/server.js
@@ -5,13 +5,16 @@ import connectDB from './configs/db.js';
 import 'dotenv/config';
 import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
+import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 
 const app=express();
 const port =process.env.PORT || 4000;
 
 await connectDB()
-
+await connectCloudinary()
 
 // allow multiple origins
 const allowedOrigin = "http://localhost:5173"; // your frontend URL
@@ -32,6 +35,10 @@ app.use(cors({
 app.get('/',(req,res)=>res.send('API is working'));
 app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+
+
 
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
